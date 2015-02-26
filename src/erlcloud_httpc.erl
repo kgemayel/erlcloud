8c4e2aeae7e42d597c05157ec727dbf767aad232
Benoit Chesneau <bchesneau@gmail.com>
2015-02-26 08:58:26 +0100

    hibernate the ssl connections after 200ms if iddle
    allows to reduce the memory usage.

--- src/erlcloud_httpc.erl
+++ src/erlcloud_httpc.erl
@@ -12,10 +12,22 @@
 -export([request/6]).
 
 -define(POOL_NAME, erlcloud_pool).
+-define(HIBENATE_AFTER, 200).
 
 request(URL, Method, Hdrs, Body, Timeout, Config) ->
+    %% set ssl options. We check the certificate using latest roots, and
+    %% hibernate the connections after ?HIBERNATE_AFTER time to reduce the
+    %% memory usage.
+    CACertFile =  filename:join(hackney_util:privdir(), "ca-bundle.crt"),
+    SSLOptions = [{cacertfile, CACertFile },
+                  {verify, verify_peer},
+                  {depth, 99},
+                  {hibernate_after, ?HIBERNATE_AFTER}],
+
+    %% connection options.
     Options = [{recv_timeout, Timeout},
-               {connect_timeout, Timeout}],
+               {connect_timeout, Timeout},
+               {ssl_options, SSLOptions}],
 
     case is_async() of
         false -> do_sync_request(URL, Method, Hdrs, Body, Options);
